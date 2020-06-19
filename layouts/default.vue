03cce6f250e716b77992c6065ced2665b03b42d0
goyasho <goyasho0831@gmail.com>
2020-06-19 16:24:34 +0900

    laytout default を composition-api で書き換えてみた

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,30 +64,47 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
+<script lang="ts">
+import { defineComponent, ref } from '~/node_modules/@vue/composition-api';
+
+type Item = {
+  icon: string;
+  title: string;
+  to: string;
+};
+
+export default defineComponent({
+  setup() {
+    const clipped = ref(false);
+    const drawer = ref(false);
+    const fixed = ref(false);
+    const items = ref<Item[]>([
+      {
+        icon: 'mdi-apps',
+        title: 'Welcome',
+        to: '/',
+      },
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire',
+        to: '/inspire',
+      },
+    ]);
+    const miniVariant = ref(false);
+    const right = ref(false);
+    const rightDrawer = ref(false);
+    const title = ref('Vuetify.js');
+
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      clipped,
+      drawer,
+      fixed,
+      items,
+      miniVariant,
+      right,
+      rightDrawer,
+      title,
     };
   },
-};
+});
 </script>
